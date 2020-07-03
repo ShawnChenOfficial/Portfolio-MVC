bf49d01b23542b529faec9e71929c393e79fc3d3
Shawn <2017003399@student.sit.ac.nz>
2020-07-03 13:23:14 +1200

    Upload Files

---
+++ Portfolio/Model/Message.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Model
+{
+    public class Message
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "First Name:")]
+        public string FirstName { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Lastname:")]
+        public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Gender:")]
+        public string Gender { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Company Name:")]
+        public string Company { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Company Region:")]
+        public string CompanyRegion { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Company Location:")]
+        public string CompanyLocation { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Your Email Address:")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Your Mobile Number:")]
+        public string Mobile { get; set; }
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Comments:")]
+        public string ExtraComment { get; set; }
+    }
+}
---
+++ Portfolio/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿$("#Top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1000);
+});
+
+$(window).on('load', UpDownArrowsSwitcher());
+$(window).resize(function () {
+    UpDownArrowsSwitcher();
+});
+
+function UpDownArrowsSwitcher() {
+    if (ToDisableNavigationArrows()) {
+        $('#Top').css({ display: 'none' });
+        $('#furtherInfo').css({ display: 'none' });
+    }
+    else {
+        $('#Top').css({ display: 'block' });
+        $('#furtherInfo').css({ display: 'block' });
+        window.scroll(0, $(document).scrollTop() + 1);
+        window.scroll(0, $(document).scrollTop() - 1);
+    }
+}
+
+function ToDisableNavigationArrows() {
+    return $(window).height() < 768 || $(window).width() < 768 || ($(window).width() < 768 && $('#Contact').parent().find('div').hasClass('border-div-active'));
+}
